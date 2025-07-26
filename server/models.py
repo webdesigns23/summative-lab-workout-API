9ed2130ca5dac2248b3edd75715f629eaf2856b5
Sharmaine Perea <sharmaine.perea23@gmail.com>
2025-07-25 19:03:30 -0600

    defined models Exercise, Workout, and WorkoutExercise

--- server/models.py
+++ server/models.py
@@ -3,3 +3,29 @@ from sqlalchemy.orm import validates
 db = SQLAlchemy()
 
 # Define Models here
+class Exercise(db.Model):
+	__tablename__ = 'exercises'
+
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String, nullable=False)
+	category = db.Column(db.String)
+	equipment_needed = db.Column(db.Boolean)
+
+class Workout(db.Model):
+	__tablename__ = 'workout'
+
+	id = db.Column(db.Integer, primary_key=True)
+	date = db.Column(db.Date)
+	duration_minutes = db.Column(db.Integer)
+	notes = db.Column(db.Text)
+
+class WorkoutExercises(db.Model):
+	__tablename__ = 'workout-exercises'
+
+	id = db.Column(db.Integer, primary_key=True)
+	workout_id = db.Column(db.Integer)
+	exercise_id = db.Column(db.Integer)
+	reps = db.Column(db.Integer)
+	sets = db.Column(db.Integer)
+	duration_seconds = db.Column(db.Integer)
+	
